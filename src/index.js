8de3c8ea7673864d35bcbf3de867710d7cbb0cb3
choco <choco@rambler-co.ru>
2018-09-04 10:36:24 +0300

    simple person card

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './hello';
+import Person from './person';
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<Person />, document.getElementById('rapp'));
---
+++ src/person.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Person() {
+    return (
+        <div className="person">
+            <h3>Max</h3>
+            <p>Your Age: 28</p>
+        </div>
+    )
+}
