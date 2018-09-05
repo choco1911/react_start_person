e8eb93cc59cabf36547c49e9ff40128cf5ad8de4
choco <choco@rambler-co.ru>
2018-09-05 11:47:34 +0300

    change state

--- src/App.js
+++ src/App.js
@@ -9,10 +9,22 @@ class App extends Component {
             {name: 'Stephanie', age:26}
         ]
     }
+
+    switchNameHandler = () => {
+//        console.log('Was clicked!');
+        this.setState({
+            persons: [
+                {name: 'Maximilian', age:28},
+                {name: 'Manu', age:29},
+                {name: 'Stephanie', age:27}
+            ]
+        })
+    }
+
     render () {
         return (
             <div className="App">
-                <button>Switch Name</button>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
                 <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
                 <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
