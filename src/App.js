49bfb6b883f7e9b7ce04642b75aba141339694bd
choco <choco@rambler-co.ru>
2018-09-08 00:39:29 +0300

    locating error inside component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Person from './Person/person'
 import './App.css'
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 class App extends Component {
     state = {
@@ -53,12 +54,14 @@ class App extends Component {
             persons = (
                 <div>
                     {this.state.persons.map((person, index) => {
-                        return <Person click={() => this.deletePersonHandler(index)}
+                        return <ErrorBoundary key={person.id}>
+                            <Person click={() => this.deletePersonHandler(index)}
                                 name={person.name}
                                 age={person.age}
                                 key={person.id}
                                 change={(event) => this.nameChangedHandler(event, person.id)}
                                 />
+                        </ErrorBoundary>
                     })}
                 </div>
             )
