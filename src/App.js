61f26a25e796ca6cc75033acb13b0b274f9afb7d
choco <choco@rambler-co.ru>
2018-09-08 13:07:33 +0300

    delete errors and back to main code

--- src/App.js
+++ src/App.js
@@ -54,14 +54,12 @@ class App extends Component {
             persons = (
                 <div>
                     {this.state.persons.map((person, index) => {
-                        return <ErrorBoundary key={person.id}>
-                            <Person click={() => this.deletePersonHandler(index)}
+                        return <Person click={() => this.deletePersonHandler(index)}
                                 name={person.name}
                                 age={person.age}
                                 key={person.id}
                                 change={(event) => this.nameChangedHandler(event, person.id)}
                                 />
-                        </ErrorBoundary>
                     })}
                 </div>
             )
