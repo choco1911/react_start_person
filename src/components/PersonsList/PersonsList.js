316bc91f82311ac378c93fab71a7089a3c2c2605
choco <choco@rambler-co.ru>
2018-09-09 19:18:29 +0300

    Life-cycle of Update componen

--- src/components/PersonsList/PersonsList.js
+++ src/components/PersonsList/PersonsList.js
@@ -15,6 +15,24 @@ class Persons extends Component {
         console.log('[PersonsList.js] Inside componentDidMount')
     }
 
+    componentWillReceiveProps (nextProps) {
+        console.log('[Update PersonsList.js] Inside componentWillReceiveProps', nextProps)
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Update PersonsList.js] Inside shouldComponentUpdate', nextProps, nextState)
+        //return false
+        return nextProps.persons !== this.props.persons
+    }
+
+    componentWillUpdate (nextProps, nextState) {
+        console.log('[Update PersonsList.js] Inside componentWillUpdate', nextProps, nextState)
+    }
+    
+    componentDidUpdate () {
+        console.log('[Update PersonsList.js] Inside componentDidUpdate')
+    }
+
     render () {
         console.log('[PersonsList.js] Inside Render Method')
         return this.props.persons.map((person, index) => {
